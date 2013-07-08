ea0da12de4314899349332807f577cfa01518e1f
taballa.hp-PD <taballa@gmail.com>
2013-07-08 13:57:37 +0800

    added translate.rb

--- lib/tablestyle.rb
+++ lib/tablestyle.rb
@@ -6,3 +6,25 @@ module Tablestyle
         puts "HELLO module!"
     end
 end
+
+class Nvshen
+  def self.hi(language = "english")
+    translator = Translator.new(language)
+    puts translator.hi
+  end
+end
+
+class Nvshen::Translator
+  def initialize(language)
+    @language = language
+  end
+
+  def hi
+    case @language
+    when "chinese"
+      "你好 女神"
+    else
+      "hello nvshen"
+    end
+  end
+end
