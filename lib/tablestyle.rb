1be6364adf8e3bf87e63bc6df902e05486eccf6b
taballa.hp-PD <taballa@gmail.com>
2013-07-10 19:15:22 +0800

    change STYLESHEETS path

--- lib/tablestyle.rb
+++ lib/tablestyle.rb
@@ -4,10 +4,7 @@ Compass::Frameworks.register("tablestyle", :path => "#{File.dirname(__FILE__)}/.
 
 module Tablestyle
     # Your code goes here...
-    STYLESHEETS = File.expand_path("../stylesheets", __FILE__)
+    STYLESHEETS = File.expand_path("../sass", __FILE__)
 
-    def Tablestyle.hi
-        puts "HELLO module!"
-    end
 end
 
