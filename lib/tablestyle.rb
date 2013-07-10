ef64e4e8afca1277ac62c72a8495efcec84ae871
taballa.hp-PD <taballa@gmail.com>
2013-07-10 11:21:36 +0800

    changed STYLESHEETS to ../stylesheets

--- lib/tablestyle.rb
+++ lib/tablestyle.rb
@@ -4,31 +4,10 @@ Compass::Frameworks.register("tablestyle", :path => "#{File.dirname(__FILE__)}/.
 
 module Tablestyle
     # Your code goes here...
-    STYLESHEETS = File.expand_path("../sass", __FILE__)
+    STYLESHEETS = File.expand_path("../stylesheets", __FILE__)
 
     def Tablestyle.hi
         puts "HELLO module!"
     end
 end
 
-class Nvshen
-  def self.hi(language = "english")
-    translator = Translator.new(language)
-    puts translator.hi
-  end
-end
-
-class Nvshen::Translator
-  def initialize(language)
-    @language = language
-  end
-
-  def hi
-    case @language
-    when "chinese"
-      "你好 女神"
-    else
-      "hello nvshen"
-    end
-  end
-end
